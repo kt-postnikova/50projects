4fcea041d02e9e4bcfda7de96d609f571a5ef3fe
kt-postnikova <kt-postnikova@yandex.ru>
2022-06-15 12:20:35 +0300

    улучшение проекта

--- expandingCards/index.js
+++ expandingCards/index.js
@@ -1,9 +1,13 @@
 const cards = document.querySelectorAll('.card');
 
+function removeActiveClass(cards) {
+  const activeCard = Array.from(cards).find(card => card.classList.contains('card_active'));
+  activeCard.classList.remove('card_active');
+}
+
 cards.forEach(card => {
   card.addEventListener('click', () => {
-    const activeCard = Array.from(cards).find(card => card.classList.contains('card_active'));
-    activeCard.classList.remove('card_active');
+    removeActiveClass(cards);
     card.classList.add('card_active');
   })
 })
